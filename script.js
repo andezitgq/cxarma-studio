a5a9c0a9592baa6bc6167657af435d6cde4a0e30
andezitgq <sanerhp.mail@gmail.com>
2022-05-28 17:11:42 +0300

    re

---
+++ script.js
@@ -0,0 +1,22 @@
+jQuery('document').ready(function() {
+
+var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-black')
+        } else {
+            navbarCollapsible.classList.add('navbar-black')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);	
+	
+});
